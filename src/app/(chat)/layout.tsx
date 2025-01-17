c0498aee6ac98f3af84e00efb408056e51056498
Shelley Pham <phamshelley@gmail.com>
2025-01-17 14:25:16 -0800

    fix main padding

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -4,15 +4,15 @@ import { AgentProvider } from './context/agent-context';
 import { SidebarContent } from '@/components/sidebar-content';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <AgentProvider>
-        <SidebarContent />
-        <main className="relative flex h-dvh w-dvw flex-col overflow-hidden">
-          <SidebarTrigger />
-          {children}
-        </main>
-      </AgentProvider>
-    </SidebarProvider>
-  );
+	return (
+		<SidebarProvider>
+			<AgentProvider>
+				<SidebarContent />
+				<main className="relative flex h-dvh w-dvw flex-col overflow-hidden space-y-2 p-2">
+					<SidebarTrigger />
+					{children}
+				</main>
+			</AgentProvider>
+		</SidebarProvider>
+	);
 }

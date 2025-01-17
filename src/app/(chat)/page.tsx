3592982a67edc84e9d74ba8db5dc506f0c296806
Shelley Pham <phamshelley@gmail.com>
2025-01-17 14:29:46 -0800

    fix message composer margins and padding

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -4,12 +4,12 @@ import { Messages } from '@/components/messages';
 import { MessageComposer } from '@/components/message-composer';
 
 export default function Home() {
-  return (
-    <div className="relative flex h-full min-w-0 flex-col gap-5 overflow-hidden bg-background py-4">
-      <div className="flex-1 overflow-auto">
-        <Messages />
-      </div>
-      <MessageComposer />
-    </div>
-  );
+	return (
+		<div className="relative flex h-full min-w-0 flex-col gap-5 overflow-hidden bg-background pt-4">
+			<div className="flex-1 overflow-auto">
+				<Messages />
+			</div>
+			<MessageComposer />
+		</div>
+	);
 }

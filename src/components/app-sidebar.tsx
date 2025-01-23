424021b4ee6d5404ddb5b86d0ecfeca158610523
Shelley Pham <phamshelley@gmail.com>
2025-01-23 01:37:50 -0800

    create visual button messages

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -11,17 +11,19 @@ import { useAgentContext } from '@/app/(chat)/context/agent-context';
 import { useAgents } from '@/components/hooks/use-agents';
 import { useIsMobile } from './hooks/use-mobile';
 import { useEffect, useRef, useState } from 'react';
+import { useAgentMessages } from './hooks/use-agent-messages';
+
 
 export function AppSidebar() {
-    return <AppSidebarContent />;
-}
-
-function AppSidebarContent() {
     const { agentId, setAgentId } = useAgentContext();
+    const { data } = useAgents();
+    const { data: messages } = useAgentMessages(agentId);
+
     const isMobile = useIsMobile();
     const { toggleSidebar } = useSidebar();
 
-    const { data } = useAgents();
+
+    console.log('data', data);
 
     return (
         <SidebarContent>
@@ -30,22 +32,25 @@ function AppSidebarContent() {
                     <SidebarMenu className="cursor-pointer">
                         {data &&
                             data.map((agent) => (
-                                <SidebarMenuItem key={agent.id}>
-                                    <SidebarMenuButton
-                                        asChild
-                                        isActive={agent.id === agentId}
-                                        className="overflow-hidden whitespace-nowrap"
-                                        onClick={() => {
-                                            if (isMobile) {
-                                                toggleSidebar();
-                                            }
-                                            setAgentId(agent.id);
-                                        }}
-                                    >
-                                        <div className="flex-1 overflow-hidden">
-                                            <span className="block w-full truncate">{agent.name}</span>
-                                        </div>
-                                    </SidebarMenuButton>
+                                <SidebarMenuItem key={agent.id} >
+                                    <div className='border-l-4 border-gray-200' >
+                                        <SidebarMenuButton
+                                            asChild
+                                            isActive={agent.id === agentId}
+                                            className="overflow-hidden whitespace-nowrap h-full"
+                                            onClick={() => {
+                                                if (isMobile) {
+                                                    toggleSidebar();
+                                                }
+                                                setAgentId(agent.id);
+                                            }}
+                                        >
+                                            <div className="flex overflow-hidden flex-col">
+                                                <span className="block w-full truncate">{agent.name}</span>
+                                                <span className="block w-full truncate text-muted-foreground">messages</span>
+                                            </div>
+                                        </SidebarMenuButton>
+                                    </div>
                                 </SidebarMenuItem>
                             ))}
                     </SidebarMenu>

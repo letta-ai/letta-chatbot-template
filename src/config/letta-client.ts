d8cdf0d13e75ed78f7b8168936524aed8be830ed
Shelley Pham <phamshelley@gmail.com>
2025-01-23 11:38:18 -0800

    fix letta token should be optional

--- src/config/letta-client.ts
+++ src/config/letta-client.ts
@@ -1,10 +1,10 @@
 import { LettaClient } from '@letta-ai/letta-client';
 
-const LETTA_TOKEN = process.env.LETTA_TOKEN;
+const LETTA_TOKEN = process.env.LETTA_TOKEN || 'DEFAULT_TOKEN';
 const BASE_URL = process.env.BASE_URL;
 
 if (!LETTA_TOKEN) {
-  throw new Error('Environment variable LETTA_TOKEN is not set.');
+  console.error('LETTTA_TOKEN is not set. You might not be able to use Letta\'s full functionality.')
 }
 
 if (!BASE_URL) {

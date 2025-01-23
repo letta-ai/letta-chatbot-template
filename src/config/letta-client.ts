ccb9314d5f05f0c6c439671357b756c0fe7fdac9
Shelley Pham <phamshelley@gmail.com>
2025-01-23 11:40:10 -0800

    base url is optional

--- src/config/letta-client.ts
+++ src/config/letta-client.ts
@@ -1,14 +1,14 @@
 import { LettaClient } from '@letta-ai/letta-client';
 
 const LETTA_TOKEN = process.env.LETTA_TOKEN || 'DEFAULT_TOKEN';
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.BASE_URL || 'http://localhost:8283';
 
 if (!LETTA_TOKEN) {
   console.error('LETTTA_TOKEN is not set. You might not be able to use Letta\'s full functionality.')
 }
 
 if (!BASE_URL) {
-  throw new Error('Environment variable BASE_URL is not set.');
+  console.error('BASE_URL is not set. We are using your localhost.');
 }
 
 const client = new LettaClient({ token: LETTA_TOKEN, baseUrl: BASE_URL });

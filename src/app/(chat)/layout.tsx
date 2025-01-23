e662c242175f37ecc809361ceedaa5cc3a1f37ed
Shelley Pham <phamshelley@gmail.com>
2025-01-23 03:06:03 -0800

    more correct

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,20 +1,17 @@
 'use client';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { AgentProvider, useAgentContext } from './context/agent-context';
+
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { useAgentContext } from './context/agent-context';
 import { SidebarContent } from '@/components/sidebar-content';
 import { ChatHeader } from '@/components/chat-header';
-import { AgentDetailsProvider } from '@/components/ui/agent-details';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useEffect } from 'react';
 import { useAgents } from '@/components/hooks/use-agents';
 import { useAgentState } from '@/components/hooks/use-agent-state';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	const { data } = useAgents();
-	const { agentId, setAgentId } = useAgentContext();
-	const { data: agentData } = useAgentState(agentId);
-	const agentName = agentData?.name;
-
+	const { setAgentId } = useAgentContext();
 	const ref = useRef(false);
 
 	useEffect(() => {
@@ -30,7 +27,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 			<main className="relative flex h-dvh w-dvw flex-col overflow-hidden">
 				<div className="flex border-b border-border p-2.5 gap-3">
 					<SidebarTrigger />
-					<ChatHeader agentName={agentName || ''} />
+					<ChatHeader />
 				</div>
 				{children}
 			</main>
--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -1,6 +1,11 @@
 import { AgentDetailsTrigger } from './ui/agent-details';
+import { useAgentContext } from '@/app/(chat)/context/agent-context';
+import { useAgentState } from './hooks/use-agent-state';
 
-export const ChatHeader: React.FC<{ agentName: string }> = ({ agentName }) => {
+export const ChatHeader: React.FC = () => {
+    const { agentId, setAgentId } = useAgentContext();
+    const { data: agentData } = useAgentState(agentId);
+    const agentName = agentData?.name;
     return (
         <div className="flex-1">
             {agentName && (

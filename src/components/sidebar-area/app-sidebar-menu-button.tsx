51019fd68077a2da1e21b66867777f1369c3f586
Shelley Pham <phamshelley@gmail.com>
2025-01-31 15:26:41 -0800

    add options button on sidebar, tooltip

--- src/components/sidebar-area/app-sidebar-menu-button.tsx
+++ src/components/sidebar-area/app-sidebar-menu-button.tsx
@@ -9,6 +9,9 @@ import { useAgentMessages } from '../hooks/use-agent-messages'
 import { SkeletonLoadBlock } from '../ui/skeleton-load-block'
 import { DEFAULT_BOT_MESSAGE, NO_MESSAGES_LABEL } from '@/app/lib/labels'
 import { AgentState } from '@letta-ai/letta-client/api'
+import { Ellipsis } from 'lucide-react'
+import { Button } from '../ui/button'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
 
 export const AppSidebarMenuButton: React.FC<{
   agent: AgentState
@@ -26,7 +29,7 @@ export const AppSidebarMenuButton: React.FC<{
         id={agent.id}
         asChild
         isActive={agent.id === agentId}
-        className='overflow-hidden whitespace-nowrap h-full'
+        className='overflow-hidden whitespace-nowrap h-full gap-0.5 text-gray-50 hover:text-black'
         onClick={() => {
           if (isMobile) {
             toggleSidebar()
@@ -34,19 +37,35 @@ export const AppSidebarMenuButton: React.FC<{
           setAgentId(agent.id)
         }}
       >
-        <div className='overflow-hidden flex-col'>
-          <span className='block w-full truncate'>{agent.name}</span>
-          <span className='block w-full truncate text-muted-foreground'>
-            {data ? (
-              data[data.length - 1].message === DEFAULT_BOT_MESSAGE ? (
-                <i>{NO_MESSAGES_LABEL}</i>
+        <div className='flex group'>
+          <div className='overflow-hidden flex-col'>
+            <span className='block w-full truncate text-primary'>{agent.name}</span>
+            <span className='block w-full truncate text-muted-foreground'>
+              {data ? (
+                data[data.length - 1].message === DEFAULT_BOT_MESSAGE ? (
+                  <i>{NO_MESSAGES_LABEL}</i>
+                ) : (
+                  `${data[data.length - 1].message}`
+                )
               ) : (
-                `${data[data.length - 1].message}`
-              )
-            ) : (
-              <SkeletonLoadBlock className='w-full h-[1.43em]' />
-            )}
-          </span>
+                <SkeletonLoadBlock className='w-full h-[1.43em]' />
+              )}
+            </span>
+          </div>
+          <div>
+            <div className='flex rounded-full'>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant='ghost' size='icon' className='w-2 h-3 p-3'>
+                    <Ellipsis />
+                  </Button>
+                  <TooltipContent>
+                    <span>Options</span>
+                  </TooltipContent>
+                </TooltipTrigger>
+              </Tooltip>
+            </div>
+          </div>
         </div>
       </SidebarMenuButton>
     </div>
